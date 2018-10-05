f38e72add8a0dddfa52ce68c78c46b48e7ab8c1f
pro101noob <baryshnikvlad@gmail.com>
2018-10-05 16:39:02 +0100

    123
    123

--- 123.c
+++ 123.c
@@ -2,9 +2,9 @@
 #include<math.h>
 
 int main() {
-  int k = 0; s = 0;
+  int k = 0, s = 0;
   scanf("%d", &k);
-  for (int i=5; i<=k; i+=5 ) s+=n/i;
+  for (int i=5; i<=k; i*=5 ) s+=k/i;
   printf("%.d\n", s);
   return 0;
 }
