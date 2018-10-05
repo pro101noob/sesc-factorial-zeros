450b0753b04a004ca5d9259a1dacf1dc7805dded
pro101noob <baryshnikvlad@gmail.com>
2018-10-05 16:36:35 +0100

    secs
    secs

--- 123.c
+++ 123.c
@@ -2,11 +2,9 @@
 #include<math.h>
 
 int main() {
-  int k = 0;
-  int s = 0;
+  int k = 0; s = 0;
   scanf("%d", &k);
-  for (int i=1; i<k+1; i++ )
-    if (i % 5 == 0) {s+=1;}
+  for (int i=5; i<=k; i+=5 ) s+=n/i;
   printf("%.d\n", s);
   return 0;
 }
